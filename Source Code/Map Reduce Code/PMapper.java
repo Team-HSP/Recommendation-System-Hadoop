97b9df02c6b3c37a16cdfe917e17f2d34bc4b2ca
Pindikanti Hitesh <hiteshpindikanti@gmail.com>
2017-11-23 23:28:13 +0530

    Update PMapper.java

--- Source Code/Map Reduce Code/PMapper.java
+++ Source Code/Map Reduce Code/PMapper.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+
 public class PMapper extends Mapper<Object,Text,Text,Text>
 	{
 		public void map(Object key,Text value,Context context) throws IOException, InterruptedException
